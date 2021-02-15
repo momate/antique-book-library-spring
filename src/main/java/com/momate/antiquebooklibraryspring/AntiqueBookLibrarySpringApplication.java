c86dca23efdf4144cf7ce316fa8186d377d30f92
momate <momate150@gmail.com>
2021-02-15 16:33:36 +0100

    Initial commit

---
+++ src/main/java/com/momate/antiquebooklibraryspring/AntiqueBookLibrarySpringApplication.java
@@ -0,0 +1,13 @@
+package com.momate.antiquebooklibraryspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiqueBookLibrarySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiqueBookLibrarySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momate/antiquebooklibraryspring/AntiqueBookLibrarySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momate.antiquebooklibraryspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiqueBookLibrarySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
