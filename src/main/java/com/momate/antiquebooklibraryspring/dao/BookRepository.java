b6ffe55d8dec7bab3b48fa28b5a9656aaf2a0e4b
momate <momate150@gmail.com>
2021-02-22 18:55:41 +0100

    Refactored the BookRepository class

--- src/main/java/com/momate/antiquebooklibraryspring/dao/BookRepository.java
+++ src/main/java/com/momate/antiquebooklibraryspring/dao/BookRepository.java
@@ -1,9 +1,22 @@
 package com.momate.antiquebooklibraryspring.dao;
 
 import com.momate.antiquebooklibraryspring.model.Book;
+import com.momate.antiquebooklibraryspring.model.Quality;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
-public interface BookRepository extends JpaRepository<Long, Book> {
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAllByAuthor(String author);
+
+    List<Book> findAllByPublishDate(LocalDate publishDate);
+
+    List<Book> findAllByPublisher(String publisher);
+
+    List<Book> findAllByQuality(Quality quality);
+
 }
